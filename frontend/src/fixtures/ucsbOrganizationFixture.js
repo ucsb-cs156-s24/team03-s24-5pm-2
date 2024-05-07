08adab0c5ac17f3402734783db97a51bb37f200e
Nevin Manimaran <nevin@ucsb.edu>
2024-05-06 21:35:48 -0700

    fixture

---
+++ frontend/src/fixtures/ucsbOrganizationFixture.js
@@ -0,0 +1,36 @@
+const ucsbOrganizationsFixtures = {
+    oneOrganization: {
+        "orgCode": "KRC",
+            //string
+        "orgTranslationShort": "KOREAN RADIO CL",
+            //string
+        "orgTranslation": "KOREAN RADIO CLUB",
+            //string
+        "inactive": false
+            //boolean
+    },
+    
+    threeOrganization: [
+        {
+            "orgCode": "KRC",
+            "orgTranslationShort": "KOREAN RADIO CL",
+            "orgTranslation": "KOREAN RADIO CLUB",
+            "inactive": false
+        },
+        {
+            "orgCode": "ZPR",
+            "orgTranslationShort": "ZETA PHI RHO",
+            "orgTranslation": "ZETA PHI RHO",
+            "inactive": false
+        },
+        {
+            "orgCode": "SKY",
+            "orgTranslationShort": "SKYDIVING CLUB",
+            "orgTranslation": "SKYDIVING CLUB AT UCSB",
+            "inactive": false
+        }
+    ]
+};
+
+
+export { ucsbOrganizationFixtures };
