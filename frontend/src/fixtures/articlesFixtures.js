31adb720481b9d5df6b53891dabe88bb6be0fa72
Nico Shinozaki <nshinozaki@csil.cs.ucsb.edu>
2024-05-08 00:47:09 -0700

    ns - Create fixtures for Articles

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,50 @@
+const articlesFixtures = {
+    oneArticle:
+    [
+      {
+       "id": 1,
+        "name": "Article 1",
+        "title": "Article 1 Title",
+        "url": "Article 1 URL",
+        "explanation": "Article 1 Explanation",
+        "email": "Article 1 Email",
+        "dateAdded": "2024-05-01T12:00:00"
+      }
+    ],
+
+    threeArticles:
+    [
+        {
+            "id": 2,
+             "name": "Article 2",
+             "title": "Article 2 Title",
+             "url": "Article 2 URL",
+             "explanation": "Article 2 Explanation",
+             "email": "Article 2 Email",
+             "dateAdded": "2024-05-02T12:00:00"
+    },
+
+        {
+            "id": 3,
+             "name": "Article 3",
+             "title": "Article 3 Title",
+             "url": "Article 3 URL",
+             "explanation": "Article 3 Explanation",
+             "email": "Article 3 Email",
+             "dateAdded": "2024-05-03T12:00:00"
+    },
+
+        {
+            "id": 4,
+             "name": "Article 4",
+             "title": "Article 4 Title",
+             "url": "Article 4 URL",
+             "explanation": "Article 4 Explanation",
+             "email": "Article 4 Email",
+             "dateAdded": "2024-05-04T12:00:00"
+    },
+        
+    ]
+};
+
+export { articlesFixtures };
