6a0b64dd540ec279b84c0774ca749f5304083e2a
Abhishek Ambastha <abhishekambastha@Abhisheks-MacBook-Pro.local>
2024-05-09 20:16:03 -0700

    aa - completed the dining commons menu item form

--- frontend/src/stories/components/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemForm.stories.js
+++ frontend/src/stories/components/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemForm.stories.js
@@ -26,7 +26,7 @@ Create.args = {
 export const Update = Template.bind({});
 
 Update.args = {
-    initialContents: ucsbDiningCommonsMenuItemsFixtures.oneMenuItem[0],
+    initialContents: ucsbDiningCommonsMenuItemsFixtures.oneDiningCommonsMenuItem[0],
     buttonLabel: "Update",
     submitAction: (data) => {
         console.log("Submit was clicked with data: ", data);
--- frontend/src/tests/components/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemForm.test.js
+++ frontend/src/tests/components/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemForm.test.js
@@ -41,7 +41,7 @@ describe("UCSBDiningCommonsMenuItemForm tests", () => {
         render(
             <QueryClientProvider client={queryClient}>
                 <Router>
-                    <UCSBDiningCommonsMenuItemForm initialContents={ucsbDiningCommonsMenuItemsFixtures.oneMenuItem} />
+                    <UCSBDiningCommonsMenuItemForm initialContents={ucsbDiningCommonsMenuItemsFixtures.oneDiningCommonsMenuItem} />
                 </Router>
             </QueryClientProvider>
         );
