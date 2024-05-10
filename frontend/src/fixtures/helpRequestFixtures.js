c68b534164ea666a31e4912d7aa103a217a8ae0e
SloggyYoggy <adrumm825@gmail.com>
2024-05-09 17:14:19 -0700

    added helpRequest fixtures

---
+++ frontend/src/fixtures/helpRequestFixtures.js
@@ -0,0 +1,38 @@
+const helpRequestFixtures = {
+    oneHelpRequest:
+    [
+        {
+            "id": 0,
+            "requesterEmail": "string",
+            "teamId": "string",
+            "tableOrBreakoutRoom": "string",
+            "requestTime": "2024-05-09T00:36:58.633Z",
+            "explanation": "string",
+            "solved": true
+          }
+    ],
+
+    threeHelpRequest:
+    [
+        {
+            "id": 2,
+             "name": "Cristino's Bakery",
+             "description": "This place is takeout only.  It may look mostly like a bakery with Mexican pastries, but it also has amazing burritos and tacos",      
+        },
+
+        {
+            "id": 3,
+             "name": "Freebirds",
+             "description": "Burrito joint, and iconic Isla Vista location"  
+        },
+
+        {
+            "id": 4,
+             "name": "Ca' Dario Cucina Italiana",
+             "description": "White tablecloth Italian restaurant, with great pasta and pizza"      
+        },
+        
+    ]
+};
+
+export { helpRequestFixtures };
--- frontend/src/fixtures/restaurantFixtures.js
+++ frontend/src/fixtures/restaurantFixtures.js
@@ -1,33 +1,46 @@
 const restaurantFixtures = {
     oneRestaurant:
     [
-      {
-       "id": 1,
-        "name": "The Habit",
-        "description": "Burgers and Fries"      
-      }
+    {
+        "id": 0,
+        "requesterEmail": "adrumm@gucsb.edu",
+        "teamId": "team10",
+        "tableOrBreakoutRoom": "10",
+        "requestTime": "2024-05-09T00:36:58.633Z",
+        "explanation": "help on team03",
+        "solved": true
+        }
     ],
 
     threeRestaurants:
     [
         {
+            "id": 0,
+            "requesterEmail": "adrumm@gucsb.edu",
+            "teamId": "team10",
+            "tableOrBreakoutRoom": "10",
+            "requestTime": "2024-05-09T00:36:58.633Z",
+            "explanation": "help on team03",
+            "solved": true
+        },
+        {
+            "id": 1,
+            "requesterEmail": "otheremember@gucsb.edu",
+            "teamId": "team10",
+            "tableOrBreakoutRoom": "10",
+            "requestTime": "2022-05-09T00:36:58.633Z",
+            "explanation": "help on team02",
+            "solved": false
+        },
+        {
             "id": 2,
-             "name": "Cristino's Bakery",
-             "description": "This place is takeout only.  It may look mostly like a bakery with Mexican pastries, but it also has amazing burritos and tacos",      
-        },
-
-        {
-            "id": 3,
-             "name": "Freebirds",
-             "description": "Burrito joint, and iconic Isla Vista location"  
-        },
-
-        {
-            "id": 4,
-             "name": "Ca' Dario Cucina Italiana",
-             "description": "White tablecloth Italian restaurant, with great pasta and pizza"      
-        },
-        
+            "requesterEmail": "memeber3@gucsb.edu",
+            "teamId": "team00",
+            "tableOrBreakoutRoom": "00",
+            "requestTime": "2023-05-09T00:36:58.633Z",
+            "explanation": "help on project",
+            "solved": true
+        }
     ]
 };
 
