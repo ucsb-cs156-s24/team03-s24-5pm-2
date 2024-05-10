3770efd6875931bc234ccd13a8a50ea9d7e239c9
kylume <123840358+kylume@users.noreply.github.com>
2024-05-09 17:11:40 -0700

    use simple regex for email instead of complex

--- frontend/src/main/components/RecommendationRequest/RecommendationRequestForm.js
+++ frontend/src/main/components/RecommendationRequest/RecommendationRequestForm.js
@@ -23,7 +23,7 @@ function RecommendationRequestForm({ initialContents, submitAction, buttonLabel 
     const isodate_regex = /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/i;
 
     // Stryker disable next-line Regex
-    const email_regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i;
+    const email_regex = /^[^@]+@[^@]+\.[^@]+$/i;
 
     return (
         <Form onSubmit={handleSubmit(submitAction)}>
