217686d75603dba163dc36ca79338832306e92c8
Abhishek Ambastha <abhishekambastha@Abhisheks-MBP.hsd1.ca.comcast.net>
2024-05-12 23:07:56 -0700

    pls work

--- frontend/src/main/utils/UCSBDiningCommonsMenuItemUtils.js
+++ frontend/src/main/utils/UCSBDiningCommonsMenuItemUtils.js
@@ -9,8 +9,10 @@ export function cellToAxiosParamsDelete(cell) {
     return {
         url: "/api/ucsbdiningcommonsmenuitem",
         method: "DELETE",
+        // Stryker disable all
         params: {
             id: cell.row.values.id
         }
+        // Stryker restore all
     }
 }
