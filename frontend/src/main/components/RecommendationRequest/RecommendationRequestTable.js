df0e0f50f0802dabc1de0d2d462d3144a55dc4e1
ukyl <53740464+ukyl@users.noreply.github.com>
2024-05-13 22:04:02 -0700

    Update RecommendationRequestTable.js
    lowercase

--- frontend/src/main/components/RecommendationRequest/RecommendationRequestTable.js
+++ frontend/src/main/components/RecommendationRequest/RecommendationRequestTable.js
@@ -11,7 +11,7 @@ export default function RecommendationRequestTable({ recs, currentUser }) {
     const navigate = useNavigate();
 
     const editCallback = (cell) => {
-        navigate(`/RecommendationRequest/edit/${cell.row.values.id}`)
+        navigate(`/recommendationrequest/edit/${cell.row.values.id}`)
     }
 
     // Stryker disable all : hard to test for query caching
@@ -19,7 +19,7 @@ export default function RecommendationRequestTable({ recs, currentUser }) {
     const deleteMutation = useBackendMutation(
         cellToAxiosParamsDelete,
         { onSuccess: onDeleteSuccess },
-        ["/api/RecommendationRequest/all"]
+        ["/api/recommendationrequest/all"]
     );
     // Stryker restore all 
 
