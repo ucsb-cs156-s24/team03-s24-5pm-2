6d1152d1940ce204e20fee10e9dcdbba5b454a13
SloggyYoggy <adrumm825@gmail.com>
2024-06-04 00:38:06 -0700

    passing webIT

--- src/test/java/edu/ucsb/cs156/example/web/HelpRequestWebIT.java
+++ src/test/java/edu/ucsb/cs156/example/web/HelpRequestWebIT.java
@@ -1,66 +1,56 @@
 package edu.ucsb.cs156.example.web;
 
+import edu.ucsb.cs156.example.WebTestCase;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
-import edu.ucsb.cs156.example.WebTestCase;
-
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ActiveProfiles("integration")
-@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class HelpRequestWebIT extends WebTestCase {
     @Test
-    public void admin_user_can_create_edit_delete_helprequest() throws Exception {
+    public void admin_user_can_create_edit_delete_help_request() throws Exception {
         setupUser(true);
 
-        page.getByText("HelpRequest").click();
+        page.getByText("Help Request").click();
 
         page.getByText("Create Help Request").click();
-        assertThat(page.getByText("Create New HelpRequest")).isVisible();
+        assertThat(page.getByText("Create New Help Request")).isVisible();
         page.getByTestId("HelpRequestForm-requesterEmail").fill("adrumm@ucsb.edu");
         page.getByTestId("HelpRequestForm-teamId").fill("5pm-5");
-        page.getByTestId("HelpRequestForm-tableOrBreakoutRoom").fill("10");
+        page.getByTestId("HelpRequestForm-tableOrBreakoutRoom").fill("table");
         page.getByTestId("HelpRequestForm-requestTime").fill("2024-01-03T00:00");
         page.getByTestId("HelpRequestForm-explanation").fill("helpReq");
-        page.getByTestId("HelpRequestForm-solved").click();
         page.getByTestId("HelpRequestForm-submit").click();
 
-        assertThat(page.getByTestId("HelpRequestTable-cell-row-0-col-requesterEmail"))
-                .hasText("adrumm@ucsb.edu");
-        assertThat(page.getByTestId("HelpRequestTable-cell-row-0-col-teamId"))
-                .hasText("5pm-5");
-        assertThat(page.getByTestId("HelpRequestTable-cell-row-0-col-tableOrBreakoutRoom"))
-                .hasText("10");
+        assertThat(page.getByTestId("HelpRequestTable-cell-row-0-col-explanation"))
+                .hasText("helpReq");
 
         page.getByTestId("HelpRequestTable-cell-row-0-col-Edit-button").click();
         assertThat(page.getByText("Edit Help Request")).isVisible();
-        page.getByTestId("HelpRequestForm-requesterEmail").fill("random@ucsb.edu");
-        page.getByTestId("HelpRequestForm-teamId").fill("6pm-6");
+        page.getByTestId("HelpRequestForm-explanation").fill("fixed");
         page.getByTestId("HelpRequestForm-submit").click();
 
-        assertThat(page.getByTestId("HelpRequestTable-cell-row-0-col-requesterEmail")).hasText("random@ucsb.edu");
-        assertThat(page.getByTestId("HelpRequestTable-cell-row-0-col-teamId")).hasText("6pm-6");
+        assertThat(page.getByTestId("HelpRequestTable-cell-row-0-col-explanation")).hasText("fixed");
 
         page.getByTestId("HelpRequestTable-cell-row-0-col-Delete-button").click();
-
-        assertThat(page.getByTestId("HelpRequestTable-cell-row-0-col-name")).not().isVisible();
+        assertThat(page.getByTestId("HelpRequestForm-cell-row-0-col-requesterEmail")).not().isVisible();
     }
 
     @Test
-    public void regular_user_cannot_create_helprequest() throws Exception {
+    public void regular_user_cannot_create_help_request() throws Exception {
         setupUser(false);
-
-        page.getByText("HelpRequest").click();
+        page.getByText("Help Request").click();
 
         assertThat(page.getByText("Create Help Request")).not().isVisible();
-        assertThat(page.getByTestId("HelpRequestTable-cell-row-0-col-name")).not().isVisible();
+        assertThat(page.getByTestId("HelpRequestTable-cell-row-0-col-requesterEmail")).not().isVisible();
     }
+
 }
